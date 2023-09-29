323c9ac2cf91e199784f2f691a6b5e83703bf3bd
Mikhail Prokhorov <imready.pro@gmail.com>
2023-09-29 23:04:12 +0300

    Edit project

---
+++ bioinformatics/semester2/test.py
@@ -0,0 +1,14 @@
+import os
+
+path = r'C:\Users\Mikhail\OneDrive\Учёба\ОКМБ\ЛР7\out\ligand_{i}\ligand_{i}_log.txt'
+
+affinities = {}
+
+for i in range(27):
+    file = open(path.format(i=i), 'r')
+    lines = file.readlines()
+    affinities[i] = (lines[24].strip().split()[1])
+    # print(f'best affinity (kcal/mol) for ligand_{i}: {affinities[i]}')
+
+for affinity in sorted(affinities.items(), key=lambda a: a[1])[::-1][:5]:
+    print(affinity)
